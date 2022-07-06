43be3230148fe7ad91f7a6148a0c87eb423520b0
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-07-06 13:16:59 +0300

    f3java!

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class T3java {
+
+    public static void main(String[] args) {
+        int CASH = 100;
+        System.out.println ((CASH) + " Начальная сумма");
+        int CASHINV = 1100;
+        System.out.println((CASHINV) + " Сумма пополнения");
+
+
+        int BONUS;
+        BONUS = CASHINV < 1000 ? CASHINV : CASHINV + (1 * CASHINV/100);
+        int TOTLCASH;
+        System.out.println (CASH + BONUS + " Общая сумма");
+    }
+}
