79927d9296ab9c54652700a5730a9a5abc2ac547
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-07-11 16:33:34 +0300

    change1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class T3java {
 
     public static void main(String[] args) {
         int CASH = 100;
-        System.out.println ((CASH) + " Начальная сумма");
+        System.out.println((CASH) + " Начальная сумма");
         int CASHINV = 1100;
         System.out.println((CASHINV) + " Сумма пополнения");
 
 
         int BONUS;
-        BONUS = CASHINV < 1000 ? CASHINV : CASHINV + (1 * CASHINV/100);
+        BONUS = CASHINV < 1000 ? CASHINV : CASHINV + (1 * CASHINV / 100);
         int TOTLCASH;
-        System.out.println (CASH + BONUS + " Общая сумма");
+        System.out.println(CASH + BONUS + " Общая сумма");
     }
 }
