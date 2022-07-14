ca951d1fe12dfc80ae551ebfc39c44f36946e4fe
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-07-14 10:09:53 +0300

    Change #2 camelcase modified

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-public class T3java {
+public class Main {
 
     public static void main(String[] args) {
-        int Cash = 100;
-        System.out.println((Cash) + " Начальная сумма");
-        int CashInv = 1100;
-        System.out.println((CashInv) + " Сумма пополнения");
+        int cash = 100;
+        System.out.println((cash) + " Начальная сумма");
+        int cashInv = 1100;
+        System.out.println((cashInv) + " Сумма пополнения");
 
 
-        int Bonus;
-        Bonus = CashInv < 1000 ? CashInv : CashInv + (1 * CashInv / 100);
-        int TotalCash;
-        System.out.println(Cash + Bonus + " Общая сумма");
+        int bonus;
+        bonus = cashInv < 1000 ? cashInv : cashInv + (1 * cashInv / 100);
+        int totalCash;
+        System.out.println(cash + bonus + " Общая сумма");
     }
 }
